9f0b5897be2a565d63f89d45ae240a44e9fc3cdb
Lee <li.qi@mindoop.com>
2017-05-04 19:02:48 +0800

    Player 改变颜色

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,11 +11,16 @@ public class PlayerController : NetworkBehaviour
         {
             return;
         }
-		
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Material>().color = Color.blue;
     }
 }
