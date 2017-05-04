bf567467b194f5ff8e20f54404c964e13f5c4b2a
Lee <li.qi@mindoop.com>
2017-05-04 18:39:10 +0800

    添加 NetworkTransform

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+		
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3f;
-		
+
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
     }
