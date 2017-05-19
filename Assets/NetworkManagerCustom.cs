9d001a92f29fcf6664c3ecc0fbe5e4569a268df2
Lee <li.qi@mindoop.com>
2017-05-19 14:20:32 +0800

    no message

--- Assets/NetworkManagerCustom.cs
+++ Assets/NetworkManagerCustom.cs
@@ -5,36 +5,23 @@ using UnityEngine.Networking;
 
 public class NetworkManagerCustom : NetworkManager
 {
-
-    private IEnumerator Start()
-    {
-        yield return new WaitForSeconds(2f);
-        StartServer();
-    }
-
-    public override void OnStartServer()
-    {
-        print("onstartserver");
-    }
-
+    
     public override void ServerChangeScene(string newSceneName)
     {
-        base.ServerChangeScene(newSceneName);
+        //base.ServerChangeScene(newSceneName);
         print("ServerChangeScene " + newSceneName);
     }
 
     public override void OnServerSceneChanged(string sceneName)
     {
         print("OnServerSceneChanged");
-
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            ServerChangeScene("offline");
+            base.ServerChangeScene("Main");
         }
     }
-
 }
